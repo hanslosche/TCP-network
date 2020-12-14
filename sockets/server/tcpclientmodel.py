19374ae37b8fc2885a0215bc540d71244bc18414
Hans Losche <hansloesche.art@gmail.com>
2020-12-13 21:15:02 -0500

    'updated'

---
+++ sockets/server/tcpclientmodel.py
@@ -0,0 +1,7 @@
+class TcpClient:
+    def __init(self, clientSock, clientAddr):
+        pass
+
+    def send(self, packetSize, msg, doEncode=True):
+        pass
+
--- sockets/server/tcplistener.py
+++ sockets/server/tcplistener.py
@@ -2,4 +2,39 @@ import socket, threading
 from tcpclient model import TcpClient
 
 class TcpListener:
+    def __init__(self, idAddr, port, packetSize, hasCommands=False):
+        pass
+
+    def run(self):
+        pass
+
+    def __acceptThread(self):
+        pass
+
+    def __clientThread(self, client):
+        pass
+
+    def cmdThread(self):
+        pass
+
+    def getTcpClient(self, sock):
+        pass
+
+    def send(self, client, msg, doEncode=True):
+        pass
+
+    def generateClientObject(self, clientSock, clientAddr):
+        pass
+
+    def serverStarted(self):
+        pass
+
+    def clientConnected(self, client):
+        pass
+
+    def clientDisconnected(self, client):
+        pass
+
+    def msgReceived(self, client, msg):
+        pass
 
